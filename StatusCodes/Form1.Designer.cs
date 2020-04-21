95dc60c7125075350e85fcb664fdc753c008d7ee
Ayoub RZIKI <a.rziki@b3gtech.com>
2020-04-21 09:57:07 +0200

    Ajoutez des fichiers projet.

---
+++ StatusCodes/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace StatusCodes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(479, 450);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(479, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(321, 450);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Save at";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(65, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(210, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(286, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 20);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "...";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(127, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ StatusCodes/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StatusCodes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    textBox1.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var ConnectionString = @"Data Source=10.2.243.138;Initial Catalog=B3G.Fawri.BankingV2;Integrated Security=false;Persist Security Info=False;MultipleActiveResultSets=true;User ID=sa;Password=@Dmin123";
+            var ClassStrings = string.Empty;
+            var FilePath = textBox1.Text;
+            //var FilePath = @"E:\V2\FawriBank\B3G.FawriSuite.Bank.Engine.Core\Audit\Consts\StatusCodes.cs";
+            // var FilePath = @"C:\StatusCodes\StatusCodes.cs";
+            var VariableName = string.Empty;
+            using (var cn = new SqlConnection(ConnectionString))
+            {
+                var query = "SELECT sc.Code,sl.DefaultValue FROM Core.StatusCode sc INNER JOIN Core.statusLabel sl ON sc.StatusLabelId=sl.Id";
+                var cmd = new SqlCommand(query, cn);
+                try
+                {
+                    cn.Open();
+                    Console.WriteLine("Connected to server");
+                    var reader = cmd.ExecuteReader();
+                    Console.WriteLine("Command executed successfuly");
+                    ClassStrings = "namespace B3G.FawriSuite.Bank.Engine.Core.Audit.Consts\n{\n\tpublic class StatusCodes\n\t{\n";
+                    Console.WriteLine("Preparing Class...");
+                    Console.WriteLine(ClassStrings);
+                    while (reader.Read())
+                    {
+                        VariableName = reader[1].ToString().Replace(".", "_");
+                        VariableName = VariableName.Replace("'", "");
+                        VariableName = VariableName.Replace(",", "");
+                        VariableName = VariableName.Replace("\"", "");
+                        VariableName = VariableName.Replace("/", "");
+                        VariableName = VariableName.Replace("(", "");
+                        VariableName = VariableName.Replace(")", "");
+                        VariableName = VariableName.Replace("-", "_");
+                        VariableName = VariableName.Replace("!", "");
+                        VariableName = VariableName.Replace(" ", "");
+                        VariableName = VariableName.Replace(":", "");
+                        VariableName = VariableName.Replace("’", "");
+                        VariableName = VariableName.Replace("°", "");
+                        VariableName = VariableName.Replace("É", "E");
+                        VariableName = VariableName.Replace("é", "e");
+                        VariableName = VariableName.Replace("è", "e");
+                        VariableName = VariableName.Replace("à", "a");
+                        VariableName = VariableName.Replace("À", "A");
+                        var prop = string.Format("\t\tpublic static string {0} = \"{1}\";\n", VariableName, reader[0].ToString());
+                        var existProperty = ClassStrings.Contains(VariableName);
+                        if (existProperty)
+                        {
+                            var t = prop.IndexOf('p');
+                            prop = prop.Insert(t, "//");
+                        }
+                        ClassStrings += prop;
+                        Console.WriteLine(prop);
+                    }
+                    ClassStrings += "\t}\n}";
+                    Console.WriteLine("\t}\n}");
+                    Console.WriteLine("Getting Data Successfuly..");
+                    cn.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            if (!Directory.Exists(FilePath.Substring(0, FilePath.LastIndexOf(@"\"))))
+            {
+                Directory.CreateDirectory(FilePath.Substring(0, FilePath.LastIndexOf(@"\")));
+                Console.WriteLine("Directory Created Successfuly..");
+            }
+            else
+                Console.WriteLine("Using Existing Directory..");
+            if (File.Exists(FilePath))
+            {
+                File.Delete(FilePath);
+                Console.WriteLine("Old File Deleted Successfuly..");
+            }
+            using (var fs = new FileStream(FilePath+ @"\StatusCodes.cs", FileMode.OpenOrCreate))
+            using (var sw = new StreamWriter(fs))
+            {
+                Console.WriteLine("New File Created Successefuly..");
+                Console.WriteLine("New File Opened Successefuly..");
+                sw.Write(ClassStrings);
+                Console.WriteLine("Data Writed In File Successfuly..");
+                Console.WriteLine("File Closed.");
+                Console.WriteLine("Done. Your File Exist Into : {0}.", FilePath);
+            }
+            Console.ReadLine();
+            richTextBox1.Text = ClassStrings;
+        }
+    }
+}
